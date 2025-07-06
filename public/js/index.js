749e6b7a26af172239ced2d65909a6e9b009c0b9
Kurtis Hight <gears2525@aol.com>
2025-07-05 18:49:09 -0700

    Revamp UI and emoji codex, add background image
    Updated CharCode.json to use emoji keys for colors and fixed double quote mapping. Added a boxed dark background image and applied it via CSS. Improved the UI layout in index.html with better structure, new sectioning, and updated controls. Refactored index.js to load the codex from CharCode.json asynchronously, removing the hardcoded mapping. Cleaned up and modernized CSS for a more cohesive appearance.

--- public/js/index.js
+++ public/js/index.js
@@ -1,80 +1,20 @@
 const dataList = document.getElementById('messages');
-const textInputData = document.getElementById('textData');
 
 const ipAddress = document.URL;
 
 const gridX = 22;
 const gridY = 6;
 const gridData = [];
-let isConnected = false;
-let data = {};
 
-const codex = {
-	A: 1,
-	B: 2,
-	C: 3,
-	D: 4,
-	E: 5,
-	F: 6,
-	G: 7,
-	H: 8,
-	I: 9,
-	J: 10,
-	K: 11,
-	L: 12,
-	M: 13,
-	N: 14,
-	O: 15,
-	P: 16,
-	Q: 17,
-	R: 18,
-	S: 19,
-	T: 20,
-	U: 21,
-	V: 22,
-	W: 23,
-	X: 24,
-	Y: 25,
-	Z: 26,
-	1: 27,
-	2: 28,
-	3: 29,
-	4: 30,
-	5: 31,
-	6: 32,
-	7: 33,
-	8: 34,
-	9: 35,
-	0: 36,
-	'!': 37,
-	'@': 38,
-	'#': 39,
-	$: 40,
-	'(': 41,
-	')': 42,
-	'-': 44,
-	'+': 46,
-	'&': 47,
-	'=': 48,
-	';': 49,
-	':': 50,
-	"'": 52,
-	'"': 53,
-	'%': 54,
-	',': 55,
-	'.': 56,
-	'/': 59,
-	'?': 60,
-	'°': 62,
-	'🟥': 63,
-	'🟧': 64,
-	'🟨': 65,
-	'🟩': 66,
-	'🟦': 67,
-	'🟪': 68,
-	'⬜️': 69,
-	'': 0,
-};
+let isConnected = false;
+
+let data = {};
+let codex = {};
+
+(async () => {
+	const response = await fetch('../CharCode.json');
+	codex = await response.json();
+})();
 
 const pushData = async data => {
 	try {
@@ -141,14 +81,6 @@ const createCard = (element, index) => {
 	const footer = document.createElement('footer');
 	footer.classList.add('card-footer');
 
-	// const save = document.createElement('a');
-	// save.classList.add('card-footer-item');
-	// save.innerText = 'Save';
-
-	// const edit = document.createElement('a');
-	// edit.classList.add('card-footer-item');
-	// edit.innerText = 'Edit';
-
 	const del = document.createElement('a');
 	del.classList.add('card-footer-item');
 	del.innerText = 'Delete';
@@ -159,9 +91,6 @@ const createCard = (element, index) => {
 	send.innerText = 'Send to Board';
 	send.onclick = () => sendEntry(element);
 
-	//TODO add functionality to edit card in card
-	// footer.appendChild(save);
-	// footer.appendChild(edit);
 	footer.appendChild(send);
 
 	footer.appendChild(del);
@@ -248,6 +177,8 @@ const submitGridData = async () => {
 };
 
 const submitTextData = async () => {
+	const textInputData = document.getElementById('textData');
+
 	data.messages.push({
 		type: 'text',
 		msg: textInputData.value,
